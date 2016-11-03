e0256396a2b53bf24282fd7e0c3e77f7e80163ab
Rivers Network <tvjpblogs@gmail.com>
2016-11-03 06:45:02 +0000

    Initial commit

---
+++ js/app.module.js
@@ -0,0 +1,10 @@
+(function (){
+    'use strict';
+    var app = angular.module('beerSearch', []);
+    
+    app.controller('beerSearchController', ['$scope', beerSearchController]);
+    
+    function beerSearchController($scope) {
+        
+    }
+})();
